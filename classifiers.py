29164175990c7d09e4907c009356f3d0a30b0fc4
jlopetegui98 <javier-alejandro.lopetegui-gonzalez@universite-paris-saclay.fr>
2024-04-09 22:27:26 +0200

    classifiers update

--- classifiers.py
+++ classifiers.py
@@ -1,46 +1,34 @@
 import numpy as np
 from scipy import optimize
-
+from sklearn.preprocessing import LabelBinarizer
+from tqdm import tqdm
 
 class KernelSVC:
-    
-    def __init__(self, C, kernel, epsilon = 1e-3):
-        self.type = 'non-linear'
+    def __init__(self, C, kernel):
         self.C = C                               
         self.kernel = kernel        
         self.alpha = None
         self.support = None
-        self.epsilon = epsilon
         self.norm_f = None
        
     
     def fit(self, X, y):
-       #### You might define here any variable needed for the rest of the code
         N = len(y)
-        print("Building K...")
         K = self.kernel(X,X)
-        print("Building K - done")
-        # Lagrange dual problem
+        
         def loss(alpha):
-             #'''--------------dual loss ------------------ '''
             return -alpha@np.ones(N) + 0.5 * (alpha*y) @ K @ (alpha*y)
 
-        # Partial derivate of Ld on alpha
         def grad_loss(alpha):
-            # '''----------------partial derivative of the dual loss wrt alpha -----------------'''
             return np.diag(y) @ K @ (alpha*y) - np.ones(N)
 
-
-        # Constraints on alpha of the shape :
-        # -  d - C*alpha  = 0
-        # -  b - A*alpha >= 0
-
-        fun_eq = lambda alpha: alpha @ y # '''----------------function defining the equality constraint------------------'''        
-        jac_eq = lambda alpha:  y  #'''----------------jacobian wrt alpha of the  equality constraint------------------'''
-        fun_ineq = lambda alpha: np.hstack((np.zeros(N), self.C*np.ones(N))) + np.hstack((alpha, -alpha))  # '''---------------function defining the inequality constraint-------------------'''     
-        jac_ineq = lambda alpha: np.vstack((np.eye(N), -1*np.eye(N))) # '''---------------jacobian wrt alpha of the  inequality constraint-------------------'''
+        fun_eq = lambda alpha: alpha @ y #equality constraint
+        jac_eq = lambda alpha:  y  #jacobian wrt alpha of the equality constraint
+        fun_ineq = lambda alpha: np.hstack((np.zeros(N), self.C*np.ones(N))) + np.hstack((alpha, -alpha))  #inequality constraint
+        jac_ineq = lambda alpha: np.vstack((np.eye(N), -1*np.eye(N))) #jacobian wrt alpha of the inequality constraint
         
-        constraints = ({'type': 'eq',  'fun': fun_eq, 'jac': jac_eq},
+        constraints = (
+            {'type': 'eq',  'fun': fun_eq, 'jac': jac_eq},
                        {'type': 'ineq', 
                         'fun': fun_ineq , 
                         'jac': jac_ineq})
@@ -52,101 +40,79 @@ class KernelSVC:
                                    constraints=constraints)
         self.alpha = optRes.x
         
-        ## Assign the required attributes
         support_idxs  = np.argwhere(~np.isclose(self.alpha, 0)).squeeze()
         self.support = X[support_idxs]
         self.alpha_support = self.alpha[support_idxs]
         self.y_support = y[support_idxs]
         margin_idxs = np.intersect1d(support_idxs, np.argwhere(~np.isclose(self.alpha, self.C)).squeeze())
-        self.margin_points = X[margin_idxs]#'''------------------- A matrix with each row corresponding to a point that falls on the margin ------------------'''
+        self.margin_points = X[margin_idxs]
         
         self.b = np.mean(y[margin_idxs] - K[np.ix_(margin_idxs , support_idxs)] @ \
-            (self.alpha_support*self.y_support))  #''' -----------------offset of the classifier------------------ '''
+            (self.alpha_support*self.y_support))
         self.norm_f = np.sqrt((self.alpha_support*self.y_support) @ \
-            K[np.ix_(support_idxs , support_idxs)] @ (self.alpha_support*self.y_support))# '''------------------------RKHS norm of the function f ------------------------------'''
+            K[np.ix_(support_idxs , support_idxs)] @ (self.alpha_support*self.y_support))
 
 
-    ### Implementation of the separting function $f$ 
     def separating_function(self,x):
-        # Input : matrix x of shape N data points times d dimension
-        # Output: vector of size N
         return self.kernel(x, self.support) @ (self.alpha_support*self.y_support)
 
     
     
     def predict(self, X):
-        """ Predict y values in {-1, 1} """
         d = self.separating_function(X)
         return 2 * (d+self.b> 0) - 1
 
-# class KernelSVC_SMO:
-#     def __init__(self, C, kernel, epsilon = 1e-3):
-#         self.type = 'non-linear'
-#         self.C = C                               
-#         self.kernel = kernel        
-#         self.alpha = None
-#         self.support = None
-#         self.epsilon = epsilon
-#         self.norm_f = None
-
-#     def fit(self, X, y):
-
-
 
 class MulticlassKernelSVC:
-    def __init__(self, C, kernel, epsilon = 1e-3):
+    def __init__(self, C, kernel):
         self.C = C
         self.kernel = kernel
-        self.epsilon = epsilon
         self.models = []
         self.classes = []
     
     def fit(self, X, y):
         self.classes = np.unique(y)
 
-        for c in self.classes:
+        for c in tqdm(self.classes):
             y_bin = 2*(y == c)-1
             y_c_idxs = np.where(y_bin == 1)[0]
             num_c = len(y_c_idxs)
             y_nc_idxs = np.where(y_bin == -1)[0]
-            # get the same number of samples for each class
-            y_nc = np.random.choice(y_nc_idxs, 5*num_c, replace=False)
+            y_nc = np.random.choice(y_nc_idxs, num_c, replace=False)
             y_bin = y_bin[np.union1d(y_c_idxs, y_nc)]
             X_bin = X[np.union1d(y_c_idxs, y_nc)]
-            model = KernelSVC(self.C, self.kernel, self.epsilon)
+            model = KernelSVC(self.C, self.kernel)
             model.fit(X_bin, y_bin)
-            # model.fit(X, y_bin)
             self.models.append(model)
     
     def predict(self, X):
         pred = np.zeros((X.shape[0], len(self.classes)))
-        for i, model in enumerate(self.models):
+        for i, model in tqdm(enumerate(self.models)):
             pred[:, i] = model.separating_function(X) + model.b
         return self.classes[np.argmax(pred, axis=1)]
 
 class OneVsOneKernelSVC:
-    def __init__(self, C, kernel, epsilon = 1e-3):
+    def __init__(self, C, kernel):
         self.C = C
         self.kernel = kernel
-        self.epsilon = epsilon
         self.models = []
         self.classes = []
 
     def fit(self, X, y):
         self.classes = np.unique(y)
-        for i, c1 in enumerate(self.classes):
+        for i, c1 in tqdm(enumerate(self.classes)):
             self.models.append([])
             for j, c2 in enumerate(self.classes[i+1:]):
                 y_bin_idxs = np.where((y == c1) | (y == c2))[0]
                 y_bin = 2*(y[y_bin_idxs] == c1)-1
                 X_bin = X[(y == c1) | (y == c2)]
-                model = KernelSVC(self.C, self.kernel, self.epsilon)
+                model = KernelSVC(self.C, self.kernel)
                 model.fit(X_bin, y_bin)
                 self.models[i].append((c1,c2,model))
     
     def predict(self, X):
         pred = np.zeros((X.shape[0], len(self.classes)))
-        for i, models in enumerate(self.models):
+        for i, models in tqdm(enumerate(self.models)):
             for c1,c2,model in models:
                 pred_ = model.predict(X)
                 for k, p in enumerate(pred_):
@@ -155,3 +121,33 @@ class OneVsOneKernelSVC:
                     else:
                         pred[k,c2] += 1
         return self.classes[np.argmax(pred, axis=1)]
+
+    
+class MultivariateKernelRidgeClassifier:      
+    def __init__(self,kernel,lmbda):
+        self.lmbda = lmbda                    
+        self.kernel = kernel
+        self.support = None
+        self.alpha = None
+        self.b = None
+    
+    def fit(self, X, y):
+        self.support = X
+        Y = LabelBinarizer().fit_transform(y)
+        self.alpha = np.zeros((X.shape[0],Y.shape[1]))
+        self.b = np.zeros(Y.shape[1])
+        K = self.kernel(X,X)
+        for i in tqdm(range(Y.shape[1])):
+            self.alpha[:,i] = np.linalg.solve(K + self.lmbda*K.shape[0]*np.eye(K.shape[0]) \
+                + np.ones(K.shape[0]).reshape(-1,1)@np.mean(K, axis=0).reshape(1,-1), Y[:,i] - \
+                    np.mean(Y[:,i]))    
+        
+            self.b[i] = np.mean(Y[:,i]) - np.mean(K @ self.alpha[:,i])
+
+        
+    def regression_function(self,x):
+        K = self.kernel(x,self.support)  
+        return K @ self.alpha
+    
+    def predict(self, X):
+        return np.argmax(self.regression_function(X)+np.expand_dims(self.b,axis=0), axis=1)
